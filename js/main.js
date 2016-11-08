8f6597d3b255bfb37eac091bb96ec70e8127f5a3
poper0 <sjmeyer98@gmail.com>
2016-11-07 22:22:37 -0500

    errors

--- js/main.js
+++ js/main.js
@@ -14,25 +14,7 @@ jQuery(document).ready(function($){
 	mainNav.on('click', function(event){
 		$(event.target).is(mainNav) && mainNav.children('ul').toggleClass('is-visible');
 	});
-/*
-	//open sign-up form
-	mainNav.on('click', '.cd-signup', signup_selected);
-	//open login-form form
-	mainNav.on('click', '.cd-signin', login_selected);
 
-	//close modal
-	formModal.on('click', function(event){
-		if( $(event.target).is(formModal) || $(event.target).is('.cd-close-form') ) {
-			formModal.removeClass('is-visible');
-		}	
-	});
-	//close modal when clicking the esc keyboard button
-	$(document).keyup(function(event){
-    	if(event.which=='27'){
-    		formModal.removeClass('is-visible');
-	    }
-    });
-*/
 	//switch from a tab to another
 	formModalTab.on('click', function(event) {
 		event.preventDefault();
@@ -40,11 +22,11 @@ jQuery(document).ready(function($){
 	});
 
 	//hide or show password
-	$('.hide-password').on('click', function(){
+	$('.show-password').on('click', function(){
 		var togglePass= $(this),
 			passwordField = togglePass.prev('input');
 		
-		( 'password' == passwordField.attr('type') ) ? passwordField.attr('type', 'text') : passwordField.attr('type', 'password');
+		( 'password' == passwordField.attr('type') ) ? passwordField.attr('type', 'password') : passwordField.attr('type', 'text');
 		( 'Hide' == togglePass.text() ) ? togglePass.text('Show') : togglePass.text('Hide');
 		//focus and move cursor to the end of input field
 		passwordField.putCursorAtEnd();
